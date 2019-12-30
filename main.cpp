a4d29d119b100ae5618a9950a3eb9e107527ff4f
Danny539 <danny539@csu.fullerton.edu>
2019-12-30 04:11:59 -0800

    Project2-Danny539

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
 // This program calculates and displays business expenses.
+// Name:business expenses calculator
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+int main()
+
+{
+  double days, hotel, meal, total;
+  std::cout << "Welcome to the business trip tracker!" << std::endl;
+  std::cout << "" << std::endl;
+
+  std::string location;
+  std::cout << "What is the business trip loctaion?" << std::endl;
+  std::cin >> location;
+
+  std::cout << "How many days will the trip take?" << std::endl;
+  std::cin >> days;
+
+  std::cout << "what is the total hotel expense?" << std::endl;
+  std::cin >> hotel;
+
+  std::cout << "What is the total meal expense?" << std::endl;
+  std::cin >> meal;
+
+  //calculate
+  total = hotel + meal;
+
+  //display
+  std::cout << "Location  " << " Days   " << "Hotel  "<< "Meal " << "Total" << std::endl;
+  std::cout <<  location <<'\t'<<  days<< '\t' << "$" << hotel << '\t' <<  "$" << meal << '\t' << "$" << total  << std::endl;
+
+  return 0;
+}
