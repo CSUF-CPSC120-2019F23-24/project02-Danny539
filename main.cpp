b6babacd8b9bc240ee2d73753e72dc20fdbe64c5
Danny539 <danny539@csu.fullerton.edu>
2019-12-31 16:14:16 -0800

    Project2-Danny539

--- main.cpp
+++ main.cpp
@@ -7,13 +7,14 @@
 int main()
 
 {
+  //This is defining the different variables
   double days, hotel, meal, total;
   std::cout << "Welcome to the business trip tracker!" << std::endl;
   std::cout << "" << std::endl;
-
+// This defining the location and allowing for it to get more than one word
   std::string location;
   std::cout << "What is the business trip loctaion?" << std::endl;
-  std::cin >> location;
+  getline(std::cin, location);
 
   std::cout << "How many days will the trip take?" << std::endl;
   std::cin >> days;
@@ -24,12 +25,12 @@ int main()
   std::cout << "What is the total meal expense?" << std::endl;
   std::cin >> meal;
 
-  //calculate
+  //calculates total by adding the hotel and meal prices
   total = hotel + meal;
 
   //display
   std::cout << "Location  " << " Days   " << "Hotel  "<< "Meal " << "Total" << std::endl;
-  std::cout <<  location <<'\t'<<  days<< '\t' << "$" << hotel << '\t' <<  "$" << meal << '\t' << "$" << total  << std::endl;
+  std::cout <<  location <<    '\t'<< days << '\t' << "$" << hotel << '\t' <<  "$" << meal << '\t' << "$" << total  << std::endl;
 
   return 0;
 }
